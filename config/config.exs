93f261624a0b0848e5a886d43b45162f6932705c
Garth Kidd <garth@garthk.com>
2020-11-13 14:26:17 +1100

    Configure opentelemetry

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,27 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# You can also supply opentelemetry resources using environment variables, eg.:
+# OTEL_RESOURCE_ATTRIBUTES=service.name=name,service.namespace=namespace
+
+config :opentelemetry, :resource,
+  service: [
+    name: "service-name",
+    namespace: "service-namespace"
+  ]
+
+config :opentelemetry,
+  tracer: :otel_tracer_default,
+  processors: [
+    otel_batch_processor: %{
+      exporter:
+        {OpenTelemetry.Honeycomb.Exporter,
+         http_module: OpenTelemetry.Honeycomb.Http.HackneyBackend,
+         http_options: [],
+         write_key: System.get_env("HONEYCOMB_WRITEKEY")}
+    }
+  ]
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
